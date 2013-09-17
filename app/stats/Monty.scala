ed4a39d8028721a54da36b94f9027e6f11c715df
pete gamache <pete@gamache.org>
2013-09-17 19:25:55 -0400

    monty work

--- app/stats/Monty.scala
+++ app/stats/Monty.scala
@@ -2,6 +2,7 @@ package stats
 import models._
 
 trait Monty {
+  val gamesByPlayer: Map[Player, List[Game]]
   val ascensionsByPlayer: Map[Player, List[Game]]
 
   /*  This one's kind of a bitch.  The awards:
@@ -21,20 +22,48 @@ trait Monty {
   // Only look at players who've ascended at least the minimum number of
   // times, for performance.
 
+  private val ascensionCountByPlayer: Map[Player, Int] =
+    ascensionsByPlayer.mapValues(_.size)
+
   private val maybeBirdie: Map[Player, List[Game]] =
-    ascensionsByPlayer.filter(_._2.size >= 2)
+    ascensionCountByPlayer
+      .filter(_._2 >= 2)
+      .map(kv => (kv._1, gamesByPlayer(kv._1)))
 
   private val maybeDoubleTop: Map[Player, List[Game]] =
-    maybeBirdie.filter(_._2.size >= 3)
+    ascensionCountByPlayer
+      .filter(_._2 >= 3)
+      .map(kv => (kv._1, gamesByPlayer(kv._1)))
 
   private val maybeHatTrick: Map[Player, List[Game]] =
-    maybeDoubleTop.filter(_._2.size >= 5)
+    ascensionCountByPlayer
+      .filter(_._2 >= 5)
+      .map(kv => (kv._1, gamesByPlayer(kv._1)))
 
   private val maybeGrandSlam: Map[Player, List[Game]] =
-    maybeHatTrick.filter(_._2.size >= 13)
+    ascensionCountByPlayer
+      .filter(_._2 >= 13)
+      .map(kv => (kv._1, gamesByPlayer(kv._1)))
 
-  private val maybeFullMonty: Map[Player, List[Game]] = maybeGrandSlam
+  private val maybeFullMonty = maybeGrandSlam
 
+
+  // Perhaps the easiest way to verify Bells is to keep track of game
+  // indices, and verify Bells, separately from verifying each award.
+  // Game indices are 0-based and seperate for each player.
+  // Only do this for players we care about.
+
+  private val indexByGame: Map[Game, Int] =
+    maybeBirdie.values.flatten.zipWithIndex.toMap
+
+  private def isBells(games: List[Game]): Boolean = {
+    val startIndex = indexByGame(games(0))
+    games
+      .zipWithIndex // => (game, i)
+      .map(gi => (gi._1, (indexByGame(gi._1) - gi._2) == startIndex)) // => (game, bells?)
+      .filter(gb => !gb._2) // remove bells games
+      .size == 0
+  }
 
 }
 
